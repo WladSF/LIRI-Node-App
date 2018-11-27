6127121b971ce9c3d43d8ae7111a3cf9d12f9f16
WladSF <wpaivasf@gmail.com>
2018-11-26 22:00:19 -0800

    Added dependencies

--- keys.js
+++ keys.js
@@ -4,3 +4,4 @@ exports.spotify = {
     id: process.env.eee8fa0b9f3644428418d143ea9fc4d7,
     secret: process.env.8bf056f4dd434c06a2ec9432bd09a9d4
 };
+
--- liri.js
+++ liri.js
@@ -1,4 +1,30 @@
+// DEPENDENCIES
+// =====================================
+// Read and set environment variables
 require("dotenv").config();
 
+// Import the node-spotify-api NPM package.
+var Spotify = require("node-spotify-api");
+
+// Import the API keys
 var keys = require("./keys.js");
+
+// Import the axios npm package.
+var axios = require("axios");
+
+// Import the moment npm package.
+var moment = require("moment");
+
+// Import the FS package for read/write.
+var fs = require("fs");
+
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the info requested (movie, concert, or song name)
+var infoName = "";
+
+// Initialize the spotify API client using my client id and secret
+var spotify = new Spotify(keys.spotify);
+
 
